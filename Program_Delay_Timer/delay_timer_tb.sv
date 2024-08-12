3e43934a5b88f97ef5fd9100204dac3ca87d5c48
Ng Chuan Seng <waelectriz@gmail.com>
2024-08-12 22:18:24 +0800

    [TB] Update Delay_Timer TB for endsimulation

--- Program_Delay_Timer/delay_timer_tb.sv
+++ Program_Delay_Timer/delay_timer_tb.sv
@@ -1,8 +1,10 @@
 `timescale 1ns/1ps
 
-module delay_timer_tb #(
-    parameter int WEIGHT_BIT_WIDTH = 8
-) ();
+module delay_timer_tb #() ();
+
+    parameter int WEIGHT_BIT_WIDTH = 8;
+    parameter int ENDTIME          = 300000;
+
     reg clk;
     reg rst_n;
     reg trigger_in;
@@ -30,60 +32,83 @@ module delay_timer_tb #(
 
         $dumpfile("delay_timer.vcd");
         $dumpvars();
-
-        // Initialize inputs
-        rst_n         = 0;
-
-        #1000;
-        weighted_bits = 10;
-        mode_a        = 0;
-        mode_b        = 0;
-        rst_n         = 1;
-        trigger_in    = 0;
-
-        #500;
-        trigger_in    = 1;
-
-        #15000;
-        trigger_in    = 0;
-
-        #15000;
-        trigger_in    = 1;
-
-        #2000;
-        trigger_in    = 0;
-
-        #2000;
-        trigger_in    = 1;
-
-        #2000;
-        trigger_in    = 0;
-
-        #20000;
-        trigger_in    = 1;
-
-        #30000;
-        trigger_in    = 0;
-
-        #2000;
-        trigger_in    = 1;
-
-        #2000;
-        trigger_in    = 0;
-
-        #4000;
-        trigger_in    = 1;
-
-        #10000;
-        rst_n         = 0;
-
-        #10000;
-        rst_n         = 1;
+        main;
     end
 
-    initial begin
-        clk = 0;
-        forever #500 clk = ~clk;
-    end
+    task automatic main;
+        fork
+            clock_gen;
+            trigger_rst_gen;
+            endsimulation;
+        join
+    endtask
+
+    task static trigger_rst_gen;
+        begin
+            // Initialize inputs
+            rst_n         = 0;
+
+            #1000;
+            weighted_bits = 10;
+            mode_a        = 0;
+            mode_b        = 0;
+            rst_n         = 1;
+            trigger_in    = 0;
+
+            #500;
+            trigger_in    = 1;
+
+            #15000;
+            trigger_in    = 0;
+
+            #15000;
+            trigger_in    = 1;
+
+            #2000;
+            trigger_in    = 0;
+
+            #2000;
+            trigger_in    = 1;
+
+            #2000;
+            trigger_in    = 0;
+
+            #20000;
+            trigger_in    = 1;
+
+            #30000;
+            trigger_in    = 0;
+
+            #2000;
+            trigger_in    = 1;
+
+            #2000;
+            trigger_in    = 0;
+
+            #4000;
+            trigger_in    = 1;
+
+            #10000;
+            rst_n         = 0;
+
+            #10000;
+            rst_n         = 1;
+        end
+    endtask
+
+    task static clock_gen;
+        begin
+            clk = 0;
+            forever #500 clk = ~clk;
+        end
+    endtask
+
+    task static endsimulation;
+        begin
+            #ENDTIME;
+            $display("-------------- THE SIMUALTION FINISHED ------------");
+            $finish;
+        end
+    endtask
 
 endmodule
