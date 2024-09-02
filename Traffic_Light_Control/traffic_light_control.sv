d81aa7922935d7d5b92154a502a54b779bada835
Ng Chuan Seng <waelectriz@gmail.com>
2024-09-02 20:54:12 +0800

    [RTL] Update RTL to v1.1

--- Traffic_Light_Control/traffic_light_control.sv
+++ Traffic_Light_Control/traffic_light_control.sv
@@ -1,5 +1,6 @@
 module traffic_light_control #(
-    parameter int SIMPLE_DESIGN = 0
+    parameter int SIMPLE_DESIGN = 0,
+    parameter int TIMER_LIMIT   = 10
 ) (
     input clk,
     input rstb
@@ -8,7 +9,9 @@ module traffic_light_control #(
     reg input_a, input_b;
     reg output_x, output_y, output_z;
 
-    traffic_light_fsm #() u_fsm (
+    traffic_light_fsm #(
+        .TIMER_LIMIT (TIMER_LIMIT)
+    ) u_fsm (
         .clk      (clk),
         .rstb     (rstb),
         .input_a  (input_a),
--- Traffic_Light_Control/traffic_light_fsm.sv
+++ Traffic_Light_Control/traffic_light_fsm.sv
@@ -1,4 +1,6 @@
-module traffic_light_fsm #() (
+module traffic_light_fsm #(
+    parameter int TIMER_LIMT = 10
+) (
     input clk,
     input rstb,
 
@@ -10,7 +12,31 @@ module traffic_light_fsm #() (
     output reg output_z  //! Green color
 );
 
-    always @(posedge clk or negedge rstb) begin: light_fsm
+    reg [3:0] timer_tick;
+
+    reg timer_clk;
+
+    always @(posedge clk or negedge rstb) begin: timer_tick_iter
+        if (~rstb) begin
+            timer_tick <= 4'b0;
+        end else begin
+            timer_tick <= timer_tick + 1'b1;
+        end
+    end
+
+    always @(posedge clk or negedge rstb) begin: timer_clk_gen
+        if (~rstb) begin
+            timer_clk <= 1'b0;
+        end else begin
+            if (timer_tick == TIMER_LIMIT) begin
+                timer_clk <= ~timer_clk;
+            end else begin
+                timer_clk <= timer_clk;
+            end
+        end
+    end
+
+    always @(posedge timer_clk or negedge rstb) begin: light_fsm
         if (~rstb) begin
             output_x <= 1'b0;
             output_y <= 1'b0;
